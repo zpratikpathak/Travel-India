14eebf1127caeb9676df983baa03dad11ec97c7b
Pratik Pathak <pratikpathak200@gmail.com>
2021-06-12 12:50:04 +0530

    Javascript simplified

--- public/javascript.js
+++ public/javascript.js
@@ -1,37 +1,27 @@
 const slideShow = document.getElementById("slideshow");
 const slides = slideShow.getElementsByTagName("video");
-
 var index = 0;
-function nextSlide() {
-  slides[index].classList.remove("active");
-  index = (index + 1) % slides.length;
-  slides[index].classList.add("active");
-}
-
-function prevSlide() {
-  slides[index].classList.remove("active");
-  index = (index - 1 + slides.length) % slides.length;
-  slides[index].classList.add("active");
-}
-
-document.getElementById("prevSlide").addEventListener("click", prevSlide);
-document.getElementById("nextSlide").addEventListener("click", nextSlide);
 
 const slideShowText = document.getElementById("slideShowText");
 const slidesText = slideShowText.getElementsByTagName("div");
-
 var i = 0;
-function nextSlideText() {
-  slidesText[i].classList.remove("active");
-  i = (i + 1) % slidesText.length;
-  slidesText[i].classList.add("active");
-}
 
-function prevSlideText() {
+document.getElementById("prevSlide").addEventListener("click", () => {
+  slides[index].classList.remove("active");
+  index = (index - 1 + slides.length) % slides.length;
+  slides[index].classList.add("active");
+
   slidesText[i].classList.remove("active");
   i = (i - 1 + slidesText.length) % slidesText.length;
   slidesText[i].classList.add("active");
-}
+});
 
-document.getElementById("prevSlide").addEventListener("click", prevSlideText);
-document.getElementById("nextSlide").addEventListener("click", nextSlideText);
+document.getElementById("nextSlide").addEventListener("click", () => {
+  slides[index].classList.remove("active");
+  index = (index + 1) % slides.length;
+  slides[index].classList.add("active");
+
+  slidesText[i].classList.remove("active");
+  i = (i + 1) % slidesText.length;
+  slidesText[i].classList.add("active");
+});
