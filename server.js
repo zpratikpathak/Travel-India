16c6946aa1ed022b1d19b6d51aba748a86f6df1c
Pratik Pathak <pratikpathak200@gmail.com>
2021-06-12 13:36:36 +0530

    patch 1

--- server.js
+++ server.js
@@ -1,14 +1,12 @@
-const express= require("express")
-const dotenv = require("dotenv")
-const path= require("path")
-dotenv.config()
-const app = express()
-
+const express = require("express");
+const dotenv = require("dotenv");
+const path = require("path");
+dotenv.config();
+const app = express();
 
 app.use("/", express.static(path.join(__dirname, "public")));
 
-
-const port= process.env.PORT
-app.listen(port,()=>{
-    console.log(`App is running on http://localhost:${port}`)
-})
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`App is running on http://localhost:${port}`);
+});
